500983c01c8e5f2c1ae06a5cb4927fc8896e0f34
ClintonBaker <c.dillon.baker@gmail.com>
2017-09-06 16:16:51 -0500

    Switched Home and Posts scenes around. Also added a little styling to NavBar

--- source/scenes/Home/Home.js
+++ source/scenes/Home/Home.js
@@ -1,57 +1,16 @@
 import { React } from '@packages';
-import { Post } from '@comps'
 import './styles/Home.css';
 
-const API_POSTS_PATH = 'https://jsonplaceholder.typicode.com/posts';
-
-const getPosts = () => {
-  return fetch(API_POSTS_PATH).then( res =>
-      res.json()).then( json => {
-        return json;
-      });
+type PropsT = {
+  children: any
 };
 
-const generatePostsList = ( posts ) => {
-  return posts.map( (postData, index) => {
-    return(
-      <Post key={index} data={ postData }/>
-    )
-  })
-}
-
-class Home extends React.Component {
-  state = {
-    posts: []
-  };
-
-  retrievePosts = () => {
-      getPosts().then( posts => {
-        console.log({ posts });
-        this.setState({
-          posts
-        });
-      });
-  };
-
-  render() {
-    const postsHaveBeenFetched = !!this.state.posts.length;
-      return (
-          <div styleName='Home'>
-            <div styleName='Home-top'>
-              <div styleName='Home-title'>Home</div>
-              <a href='#' styleName='Search-button' onClick={this.retrievePosts}>search</a>
-            </div>
-            <hr/>
-            <div styleName='Posts'>
-            {
-              postsHaveBeenFetched
-              ? generatePostsList(this.state.posts)
-              : <small>I got nothin</small>
-            }
-            </div>
-          </div>
-      )
-  }
+const Home = (props: PropsT) => {
+  return (
+    <div styleName="Home">
+      <p>This is Home.</p>
+    </div>
+  );
 };
 
 export default Home;
--- source/scenes/Posts/Posts.js
+++ source/scenes/Posts/Posts.js
@@ -1,16 +1,57 @@
 import { React } from '@packages';
+import { Post } from '@comps'
 import './styles/Posts.css';
 
-type PropsT = {
-  children: any
+const API_POSTS_PATH = 'https://jsonplaceholder.typicode.com/posts';
+
+const getPosts = () => {
+  return fetch(API_POSTS_PATH).then( res =>
+      res.json()).then( json => {
+        return json;
+      });
 };
 
-const Posts = (props: PropsT) => {
-  return (
-    <div styleName="Posts">
-      <p>This is Posts.</p>
-    </div>
-  );
+const generatePostsList = ( posts ) => {
+  return posts.map( (postData, index) => {
+    return(
+      <Post key={index} data={ postData }/>
+    )
+  })
+}
+
+class Posts extends React.Component {
+  state = {
+    posts: []
+  };
+
+  retrievePosts = () => {
+      getPosts().then( posts => {
+        console.log({ posts });
+        this.setState({
+          posts
+        });
+      });
+  };
+
+  render() {
+    const postsHaveBeenFetched = !!this.state.posts.length;
+      return (
+          <div styleName='Posts'>
+            <div styleName='Posts-top'>
+              <div styleName='Posts-title'>All My Blag Posts:</div>
+              <a href='#/posts' styleName='Search-button' onClick={this.retrievePosts}>search</a>
+            </div>
+            <hr/>
+            <div styleName='Posts-list'>
+            {
+              postsHaveBeenFetched
+              ? generatePostsList(this.state.posts)
+              : <small>I got nothin</small>
+            }
+            </div>
+          </div>
+      )
+  }
 };
 
 export default Posts;
