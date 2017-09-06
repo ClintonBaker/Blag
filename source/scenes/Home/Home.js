ea748ba22aebebe586265e35319e5dcf1c610305
ClintonBaker <c.dillon.baker@gmail.com>
2017-09-06 17:03:09 -0500

    Added content for Home

--- source/scenes/Home/Home.js
+++ source/scenes/Home/Home.js
@@ -8,7 +8,14 @@ type PropsT = {
 const Home = (props: PropsT) => {
   return (
     <div styleName="Home">
-      <p>This is Home.</p>
+      <div styleName='Home-title'>
+        <h2>Blag</h2>
+      </div>
+      <div styleName='Home-content'>
+        <p>Hello, and welcome to the Blag! This is a super professional blag created
+          to be super awesome. I love my blag, and I'm sure you will too! This blag
+          is full of posts created with nothing but Lorem Ipsum! Impressive huh??</p>
+      </div>
     </div>
   );
 };
